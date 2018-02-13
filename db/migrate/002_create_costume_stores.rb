8bd129485d119892847db6cd98f48c108eaa657b
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 17:42:35 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
       t.boolean :in_business
       t.integer :opening_time
       t.integer :closing_time
