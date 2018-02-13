747ed99ae353b7c7016adc6196e155174d7edf76
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 17:38:26 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,8 +13,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.integer :price
+      t.integer :size
       t.string :image_url
+      t.timestamps
     end
   end
 end
