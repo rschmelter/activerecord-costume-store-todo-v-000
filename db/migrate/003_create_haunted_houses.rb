393bf50b3d4af0a640a781eb8e1f609099753005
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 17:53:53 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
       t.timestamps
     end
   end
